2f2ba47d143a002d6e1bb5755705e8b14603e06b
taodx09094 <taodxph09094@gmail.com>
2024-02-09 21:12:55 +0700

    fix

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,60 +1,36 @@
 import React from "react";
 import bgImg1 from "../assets/images/background/chibi-bg-2024.png";
 import phaoHoa from "../assets/gif/phaoHoa.gif";
-import bgTroChoi from "../assets/images/games/bg-trochoi.png";
-import { Modal } from "antd";
-import { useState } from "react";
 import RutLiXi from "../components/RutLiXi";
 const LayoutMain = () => {
-    const [openWheel, setOpenWheel] = useState(true);
-    const closeWheel = () => {
-        setOpenWheel(!openWheel);
-    };
     return (
         <>
             <div className="layout-main">
                 <img src={bgImg1} alt="" />
             </div>
-            <div className="phao-hoa-left"></div>
-            <div className="phao-hoa-right"></div>
-
-            {openWheel && (
-                <div
-                    style={{
-                        position: "absolute",
-                        top: "50%",
-                        left: "50%",
-                        transform: "translate(-50%, -50%)",
-                        zIndex: 100,
-                    }}
-                >
-                    <RutLiXi />
+            <img src={phaoHoa} className="phao-hoa-left" alt="" />
+            <img src={phaoHoa} className="phao-hoa-right" alt="" />
+            {/* <div className="phao-hoa">
+                <div className="phao-hoa-container">
+                    <img src={phaoHoa} className="phao-hoa-left" alt="" />
                 </div>
-            )}
-            {/* <Modal
-                title={null}
-                open={openWheel}
-                footer={null}
-                onCancel={closeWheel}
-                className="custom-modal"
-                zIndex={1}
+                <div className="phao-hoa-container-2">
+                    <img src={phaoHoa} className="phao-hoa-right" alt="" />
+                </div>
+            </div> */}
+
+
+            <div
+                style={{
+                    position: "absolute",
+                    top: "50%",
+                    left: "50%",
+                    transform: "translate(-50%, -50%)",
+                    zIndex: 100,
+                }}
             >
-                <img src={bgTroChoi} alt="" />
-            </Modal>
-
-            {openWheel && (
-                <div
-                    style={{
-                        position: "absolute",
-                        top: "50%",
-                        left: "50%",
-                        transform: "translate(-50%, -50%)",
-                        zIndex: 100,
-                    }}
-                >
-                    <RutLiXi />
-                </div>
-            )} */}
+                <RutLiXi />
+            </div>
         </>
     );
 };
